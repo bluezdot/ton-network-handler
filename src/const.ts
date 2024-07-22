f7ec7a6e7e0293d5a99adb0e0717a0c583bfd009
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-07-22 10:28:38 +0700

    test: add test balance for 2 testing accounts

---
+++ src/const.ts
@@ -0,0 +1,4 @@
+export const WORKCHAIN = 0;
+export const MNEMONIC = 'detect deliver invest lamp above genre either life ski sign eight subject mercy crowd cabbage bomb sniff lens tide rookie chase bid tent hello';
+export const ADDRESS_TEST_1 = 'UQBbamQ89OMv8DkcR2RW_tW9sX7xClXLt8KYDHbUqtB4dbz1';
+export const ADDRESS_TEST_2 = 'UQCDV2euuWnDhFhy1GM_u6_fIPkvfO0frg95gErAnzMqGV03';
--- src/test/test_address.ts
+++ src/test/test_address.ts
@@ -1,10 +1,13 @@
 import {getTonAddress, getTonClient, getWalletFromMnemonic} from "../base-utils";
+import {ADDRESS_TEST_1, ADDRESS_TEST_2} from "../const";
 
 // 2.1. Create transaction
 async function main () {
     const client = await getTonClient(true);
-    const address = await getTonAddress('UQBbamQ89OMv8DkcR2RW_tW9sX7xClXLt8KYDHbUqtB4dbz1');
+    const address = await getTonAddress(ADDRESS_TEST_1);
     const balance = await client.getBalance(address);
+    console.log('balance:', balance);
+    console.log('balance 2:', await client.getBalance(await getTonAddress(ADDRESS_TEST_2)));
     console.log(address.toRawString(), address.toString(), address.workChain, address.toRaw(), address.toStringBuffer());
 
     return 0;
