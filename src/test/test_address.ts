696d4d3bb5d321ce895f7c72bec6ce3cb57084f9
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-07-30 12:11:03 +0700

    test: update test address

--- src/test/test_address.ts
+++ src/test/test_address.ts
@@ -1,15 +1,32 @@
 import {getTonAddress, getTonClient, getWalletFromMnemonic} from "../base-utils";
 import {ADDRESS_TEST_1, ADDRESS_TEST_2} from "../const";
-import {fromNano} from "@ton/ton";
+import {beginCell, fromNano, storeMessage, TonClient} from "@ton/ton";
 
 // 2.1. Create transaction
 async function main () {
-    const client = await getTonClient(true);
+    // const client = await getTonClient();
+    const client = new TonClient({ endpoint: 'https://toncenter.com/api/v2/jsonRPC'})
+
     const address = await getTonAddress(ADDRESS_TEST_1);
     const balance = await client.getBalance(address);
-    console.log('balance:', fromNano(balance));
+    console.log('balance 1:', fromNano(balance));
     console.log('balance 2:', await client.getBalance(await getTonAddress(ADDRESS_TEST_2)));
     console.log(address.toRawString(), address.toString(), address.workChain, address.toRaw(), address.toStringBuffer());
+
+    const state = await client.getContractState(address);
+    console.log('[i] State', state);
+    if (!state || !state.lastTransaction) {
+        console.log('[i] No state')
+    } else {
+        const { hash: lastHash, lt: lastLt } = state.lastTransaction;
+        const lastTx = await client.getTransaction(address, lastLt, lastHash);
+        console.log('[i] lastTx', lastTx);
+        if (lastTx && lastTx.inMessage) {
+            const msgCell = beginCell().store(storeMessage(lastTx.inMessage)).endCell();
+            const inMsgHash = msgCell.hash().toString('base64');
+            console.log('[i] InMsgHash', inMsgHash);
+        }
+    }
 
     return 0;
 }
